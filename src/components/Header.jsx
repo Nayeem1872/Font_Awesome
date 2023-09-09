04480bae31f169f9c3acb1ef1ea2daeb7ae44ff1
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-09-09 07:12:06 +0600

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,26 @@ const Header = () => {
   return (
     <header className="container  p-6 flex justify-between">
       {/* Logo on the left */}
-      <div className="text-2xl font-bold flag-custom-width">
-        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" className="">
-          <style>fill:#2a5aac</style>
-          <path d="M48 24C48 10.7 37.3 0 24 0S0 10.7 0 24V64 350.5 400v88c0 13.3 10.7 24 24 24s24-10.7 24-24V388l80.3-20.1c41.1-10.3 84.6-5.5 122.5 13.4c44.2 22.1 95.5 24.8 141.7 7.4l34.7-13c12.5-4.7 20.8-16.6 20.8-30V66.1c0-23-24.2-38-44.8-27.7l-9.6 4.8c-46.3 23.2-100.8 23.2-147.1 0c-35.1-17.6-75.4-22-113.5-12.5L48 52V24zm0 77.5l96.6-24.2c27-6.7 55.5-3.6 80.4 8.8c54.9 27.4 118.7 29.7 175 6.8V334.7l-24.4 9.1c-33.7 12.6-71.2 10.7-103.4-5.4c-48.2-24.1-103.3-30.1-155.6-17.1L48 338.5v-237z"/>
-        </svg>
+      <div className="text-2xl font-bold flag-custom-width cursor-pointer">
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+  <path
+    d="M448 48V384c-63.1 22.5-82.3 32-119.5 32c-62.8 0-86.6-32-149.3-32c-20.6 0-36.6 3.6-51.2 8.2v-64c14.6-4.6 30.6-8.2 51.2-8.2c62.7 0 86.5 32 149.3 32c20.4 0 35.6-3 55.5-9.3v-208c-19.9 6.3-35.1 9.3-55.5 9.3c-62.8 0-86.6-32-149.3-32c-50.8 0-74.9 20.6-115.2 28.7V448c0 17.7-14.3 32-32 32s-32-14.3-32-32V64C0 46.3 14.3 32 32 32s32 14.3 32 32V76.7c40.3-8 64.4-28.7 115.2-28.7c62.7 0 86.5 32 149.3 32c37.1 0 56.4-9.5 119.5-32z"
+    fill="#2b5191"
+  />
+
+ 
+  <animateTransform
+    attributeType="xml"
+    attributeName="transform"
+    type="scale"
+    from="1"
+    to="1.2"
+    begin="mouseover"
+    end="mouseout"
+    dur="0.3s"
+    repeatCount="indefinite"
+  />
+</svg>
       </div>
 
       {/* Navigation links in one line */}
@@ -28,7 +43,7 @@ const Header = () => {
 
       {/* Sign In button on the right */}
       <button className="text-white py-2 px-4 rounded">
-        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+        <svg xmlns="http://www.w3.org/2000/svg" height="1.3em" viewBox="0 0 512 512">
           <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/>
         </svg>
       </button>
