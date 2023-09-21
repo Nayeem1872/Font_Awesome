abe32a6ebb3beeea3225bda654a079d8a52b0bbd
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-09-22 03:04:42 +0600

    done

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Csbf from './components/Csbf';
 import Header from './components/Header';
 import Main from './components/Main';
 import Search from './components/Search';
+import iconsData from './iconData';
 
 function App() {
   const [filter, setFilter] = useState('');
 
-  const handleCategoryFilter = (category) => {
-    setFilter(category);
-  };
+    // Filtering Input Items
+    const filteredItems = iconsData.filter(
+      (icon) =>
+        icon.text.toLowerCase().indexOf(filter.toLowerCase()) !== -1
+    );
+  
+    // Applying selected filter
+    const filteredIcons = filteredItems.filter(
+      (icon) =>
+        !filter ||
+        icon.category === filter ||
+        icon.featured === filter
+    );
+
+   
+
   return (
    <>
     <Header />
